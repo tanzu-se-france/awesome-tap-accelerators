247df13d496d2429d287299f8051766c19225d60
Alexandre Roman <alexandre.roman@broadcom.com>
2024-04-03 09:49:38 +0200

    Simplify TodoController implementation

--- java-spring-boot-basic/src/main/java/com/vmware/tanzu/tap/accelerators/springboot/todo/ExternalAPIClient.java
+++
@@ -1,9 +0,0 @@
-package com.vmware.tanzu.tap.accelerators.springboot.todo;
-
-import org.springframework.http.ResponseEntity;
-
-public interface ExternalAPIClient {
-    ResponseEntity<String> getDataFromExternalAPI();
-    ResponseEntity<String> getTodoByIdFromExternalAPI(Long id);
-}
-
--- java-spring-boot-basic/src/main/java/com/vmware/tanzu/tap/accelerators/springboot/todo/TodoController.java
+++ java-spring-boot-basic/src/main/java/com/vmware/tanzu/tap/accelerators/springboot/todo/TodoController.java
@@ -1,26 +1,29 @@
 package com.vmware.tanzu.tap.accelerators.springboot.todo;
-import org.springframework.http.ResponseEntity;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
+
+import java.util.Map;
 
 @RestController
-public class TodoController {
+class TodoController {
+    private final String todosApiUrl;
 
-    private final ExternalAPIClient externalAPIClient;
-
-    public TodoController(ExternalAPIClient externalAPIClient) {
-        this.externalAPIClient = externalAPIClient;
+    TodoController(@Value("${app.services.todos}") String todosApiUrl) {
+        this.todosApiUrl = todosApiUrl;
     }
 
     @GetMapping("/todos")
-    public ResponseEntity<String> getDataFromExternalAPI() {
-        ResponseEntity<String> response = externalAPIClient.getDataFromExternalAPI();
-        return response;
+    String call() {
+        return RestClient.create().get().uri(todosApiUrl).retrieve().body(String.class);
     }
+
     @GetMapping("/todos/{id}")
-    public ResponseEntity<String> getTodoById(@PathVariable("id") Long id) {
-        ResponseEntity<String> response = externalAPIClient.getTodoByIdFromExternalAPI(id);
-        return response;
+    String callById(@PathVariable("id") long id) {
+        final var args = Map.of("id", id);
+        return RestClient.create().get().uri(todosApiUrl + "/{id}", args).retrieve().body(String.class);
     }
 }
